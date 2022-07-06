a3f536c018156ff8999ce698ed2eafb65fb40c84
PARTH CHANDAN <75495326+ParthChandanPC@users.noreply.github.com>
2022-07-06 19:53:07 +0530

    Adding views of IDE

--- OJ/views.py
+++ OJ/views.py
@@ -4,9 +4,7 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .forms import RegisterForm
 from django.contrib.auth.decorators import login_required
-
-
-
+import requests
 def loginUser(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,7 +18,6 @@ def loginUser(request):
             return render(request, 'login.html')
     return render(request,'login.html')
     # login function
-
 def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -32,11 +29,18 @@ def register(request):
     else:
         form = RegisterForm()
     return render(request,'registration.html', {'form': form})
-
 def logoutUser(request):
     logout(request)
     return redirect('login')
-
 @login_required(login_url='login')
 def home(request):
-    return render(request,'home.html')
+    data = requests.get('http://127.0.0.1:8000/api/v1/problems?format=json').json()
+    return render(request,'home.html',{'data':data})
+@login_required(login_url='login')
+def problem_view(request,id):
+    data = requests.get(f'http://127.0.0.1:8000/api/v1/problems/{id}?format=json').json()
+    return render(request,'problem_view.html',{'data':data}) 
+
+
+def ide(request):
+    return render(request,'ide.html')
