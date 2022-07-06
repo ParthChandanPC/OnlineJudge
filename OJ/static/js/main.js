5c27aebeb7064dd9ceebd8f6e6072a3750093235
PARTH CHANDAN <75495326+ParthChandanPC@users.noreply.github.com>
2022-07-06 17:52:04 +0530

    Update main.js

--- OJ/static/js/main.js
+++ OJ/static/js/main.js
@@ -1,6 +1,4 @@
 let editor;
-// const element = document.getElementById("editor");
-// console.log(element)
 document.getElementById('editor').style.fontSize='16px';
 window.onload = function() {
     editor = ace.edit("editor");
@@ -10,11 +8,9 @@ window.onload = function() {
 
 console.log("Hello World!");
 
-
 function changeLanguage() {
 
     let language = $("#languages").val();
-
     if(language == 'c' || language == 'cpp')editor.session.setMode("ace/mode/c_cpp");
     else if(language == 'python')editor.session.setMode("ace/mode/python");
     else if(language == 'javascipt')editor.session.setMode("ace/mode/javascript");
