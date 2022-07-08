8ac2502d84cfe85b14902e5134ac4174037027d0
PARTH CHANDAN <75495326+ParthChandanPC@users.noreply.github.com>
2022-07-09 00:12:38 +0530

    Update settings.py

--- OnlineJudge/settings.py
+++ OnlineJudge/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'OJ',
     'froala_editor',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
