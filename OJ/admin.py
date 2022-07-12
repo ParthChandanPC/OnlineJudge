f92d388936550cdd526dedd44d33847575f48b4c
PARTH CHANDAN <75495326+ParthChandanPC@users.noreply.github.com>
2022-07-12 23:07:15 +0530

    Update admin.py

--- OJ/admin.py
+++ OJ/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from OJ.models import User, Problem, TestCases,Submissions
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Problem)
+admin.site.register(TestCases)
+admin.site.register(Submissions)
