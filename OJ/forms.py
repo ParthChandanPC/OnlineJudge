5ea13ec823dcfe3de2864d6d3d4b8afe466788ef
PARTH CHANDAN <75495326+ParthChandanPC@users.noreply.github.com>
2022-07-12 23:05:54 +0530

    Update forms.py

--- OJ/forms.py
+++ OJ/forms.py
@@ -1,11 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model
-User = get_user_model()
-
+from .models import User
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
 
     class Meta:
         model = User
