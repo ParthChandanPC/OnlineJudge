b10f434c66aa9220d93a8e08c412a0e430c103e5
PARTH CHANDAN <75495326+ParthChandanPC@users.noreply.github.com>
2023-07-14 22:16:11 +0530

    Update wsgi.py

--- OnlineJudge/wsgi.py
+++ OnlineJudge/wsgi.py
@@ -11,4 +11,3 @@ import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OnlineJudge.settings")
 application = get_wsgi_application()
-app = application
